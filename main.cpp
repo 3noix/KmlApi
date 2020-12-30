bd16b5409c06d9b31e87f5f8de709223954b6173
3noix <nic.trannois@free.fr>
2020-12-30 23:23:15 +0100

    Use auto instead of std::unique_ptr<T>

--- main.cpp
+++ main.cpp
@@ -14,29 +14,29 @@ int main(int argc, char *argv[])
 	QCoreApplication app{argc,argv};
 	Q_UNUSED(app)
 
-	KmlFile kml{"My kml test file"};
+	KmlFile kmlFile{"My kml test file"};
 
 	// point 1
-	std::unique_ptr<KmlPoint> p1 = std::make_unique<KmlPoint>("Departure", Point{1.37,43.63,151}, "#paddleGreenDot", AltitudeMode::ClampToGround);
-	kml.addItem(std::move(p1));
+	auto pt1 = std::make_unique<KmlPoint>("Departure", Point{1.37,43.63,151}, "#paddleGreenDot", AltitudeMode::ClampToGround);
+	kmlFile.addItem(std::move(pt1));
 
 	// point 2
-	std::unique_ptr<KmlPoint> p2 = std::make_unique<KmlPoint>("Arrival", Point{1.47,43.63,163}, "#paddleRedDot", AltitudeMode::ClampToGround);
-	kml.addItem(std::move(p2));
+	auto pt2 = std::make_unique<KmlPoint>("Arrival", Point{1.47,43.63,163}, "#paddleRedDot", AltitudeMode::ClampToGround);
+	kmlFile.addItem(std::move(pt2));
 
 	// trajectory
-	std::unique_ptr<KmlTrajectory> traj = std::make_unique<KmlTrajectory>("Trajectory", "#blueLine5", AltitudeMode::ClampToGround);
+	auto traj = std::make_unique<KmlTrajectory>("Trajectory", "#blueLine5", AltitudeMode::ClampToGround);
 	traj->addPoint(Point{1.37,43.63,151});
 	traj->addPoint(Point{1.37,43.53,154});
 	traj->addPoint(Point{1.47,43.53,158});
 	traj->addPoint(Point{1.47,43.63,162});
-	kml.addItem(std::move(traj));
+	kmlFile.addItem(std::move(traj));
 
 	// folder test
-	std::unique_ptr<KmlPoint> p3 = std::make_unique<KmlPoint>("Point 3", Point{1.335454,43.586060,151.539028}, "#paddleGreenDot", AltitudeMode::ClampToGround);
-	std::unique_ptr<KmlFolder> folder = std::make_unique<KmlFolder>("TestFolder");
-	folder->addItem(std::move(p3));
-	kml.addItem(std::move(folder));
+	auto pt3 = std::make_unique<KmlPoint>("Point 3", Point{1.335454,43.586060,151.539028}, "#paddleGreenDot", AltitudeMode::ClampToGround);
+	auto folder = std::make_unique<KmlFolder>("TestFolder");
+	folder->addItem(std::move(pt3));
+	kmlFile.addItem(std::move(folder));
 
 	// write file
 	QString filePath = QCoreApplication::applicationDirPath() + "/test.kml";
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 		std::cin.get();
 		return 1;
 	}
-	file.write(kml.toString().toUtf8());
+	file.write(kmlFile.toString().toUtf8());
 	file.close();
 	
 	// the end
